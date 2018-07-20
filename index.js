b663d1b7b4d2ad97aef63677be041035b02f3388
Brian Zhang <brianzhangw@gmail.com>
2018-07-20 03:31:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,6 +27,12 @@ function makeInt(n){
   return parseInt(n, 10)
 }
 
-function makeInt(n){
-  return('ada', 10)
+function preserveDecimal(n){
+  if (n == String){
+    return NaN
+  }
+  else
+  {
+    return parseFloat(n)  
+  }
 }
